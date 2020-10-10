936c66e7677c8f060334bf5de53c048ef8fa8443
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-10-09 21:42:51 -0500

    Almost ready...trying to understand audio drivers

--- app/src/main/cpp/my_main_lib.cpp
+++ app/src/main/cpp/my_main_lib.cpp
@@ -1,11 +1,44 @@
 #include <jni.h>
 #include <string>
 #include <fluidsynth.h>
+#include <unistd.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myappinc_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    fluid_settings_t *settings = new_fluid_settings();
+    fluid_synth_t *synth = new_fluid_synth(settings);
+    fluid_audio_driver_t *adriver = new_fluid_audio_driver(settings, synth);
+
+    fluid_synth_noteon(synth, 0, 60, 127);
+    fluid_synth_noteon(synth, 0, 64, 127);
+    fluid_synth_noteon(synth, 0, 67, 127);
+    sleep(1);
+    fluid_synth_noteoff(synth, 0, 60);
+    fluid_synth_noteoff(synth, 0, 127);
+    fluid_synth_noteoff(synth, 0, 67);
+
+    fluid_synth_noteon(synth, 0, 62, 127);
+    fluid_synth_noteon(synth, 0, 65, 127);
+    fluid_synth_noteon(synth, 0, 69, 127);
+    sleep(1);
+    fluid_synth_noteoff(synth, 0, 62);
+    fluid_synth_noteoff(synth, 0, 65);
+    fluid_synth_noteoff(synth, 0, 69);
+
+    fluid_synth_noteon(synth, 0, 64, 127);
+    fluid_synth_noteon(synth, 0, 67, 127);
+    fluid_synth_noteon(synth, 0, 71, 127);
+    sleep(1);
+    fluid_synth_noteoff(synth, 0, 127);
+    fluid_synth_noteoff(synth, 0, 67);
+    fluid_synth_noteoff(synth, 0, 71);
+
+    delete_fluid_audio_driver(adriver);
+    delete_fluid_synth(synth);
+    delete_fluid_settings(settings);
+
     std::string hello = "Hello World! Brought by C++";
     return env->NewStringUTF(hello.c_str());
 }
