9af53d650d4e16e5de85da75b5cbf29de7421705
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-10-09 23:18:21 -0500

    Salio

--- app/src/main/cpp/my_main_lib.cpp
+++ app/src/main/cpp/my_main_lib.cpp
@@ -6,10 +6,22 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myappinc_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject /* this */,
+        jstring jSoundfontPath) {
     fluid_settings_t *settings = new_fluid_settings();
     fluid_synth_t *synth = new_fluid_synth(settings);
     fluid_audio_driver_t *adriver = new_fluid_audio_driver(settings, synth);
+
+    const char *soundfontPath = env->GetStringUTFChars(jSoundfontPath, nullptr);
+    int sfont_id = fluid_synth_sfload(synth, soundfontPath, 1);
+    if (sfont_id == FLUID_FAILED)
+    {
+        delete_fluid_audio_driver(adriver);
+        delete_fluid_synth(synth);
+        delete_fluid_settings(settings);
+        std::string hello = "Error";
+        return env->NewStringUTF(hello.c_str());
+    }
 
     fluid_synth_noteon(synth, 0, 60, 127);
     fluid_synth_noteon(synth, 0, 64, 127);
--- app/src/main/java/com/example/myappinc/MainActivity.java
+++ app/src/main/java/com/example/myappinc/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.myappinc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +25,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        String path;
+        try {
+            String fileName = "my_temp_file.sf2";
+            writeFileToPrivateStorage(getAssets().open("example.sf2"), fileName);
+            path = getApplicationContext().getFilesDir().toString() + "/" + fileName;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String myFinalString = stringFromJNI(path) + " " + path;
+        tv.setText(myFinalString);
+    }
+
+    public void writeFileToPrivateStorage(InputStream is, String toFile) throws IOException {
+        int bytes_read;
+        byte[] buffer = new byte[4096];
+        FileOutputStream fos = openFileOutput(toFile, Context.MODE_PRIVATE);
+        while ((bytes_read = is.read(buffer)) != -1)
+            fos.write(buffer, 0, bytes_read); // write
+
+        fos.close();
+        is.close();
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String stringFromJNI(String path);
 }
