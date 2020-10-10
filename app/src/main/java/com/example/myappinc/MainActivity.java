18c12a153ac6028868241b9d25e270feaba80644
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-10-09 20:56:20 -0500

    Organizing

--- app/src/main/java/com/example/myappinc/MainActivity.java
+++ app/src/main/java/com/example/myappinc/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("my_main_lib");
     }
 
     @Override
