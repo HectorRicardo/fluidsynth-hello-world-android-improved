90980cb20c658c2d638bbba198974a3f022935ca
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-10-09 21:36:33 -0500

    Removing gperf and reorganizing

--- app/src/main/cpp/my_main_lib.cpp
+++ app/src/main/cpp/my_main_lib.cpp
@@ -1,13 +1,11 @@
 #include <jni.h>
 #include <string>
-#include <gperf.h>
 #include <fluidsynth.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myappinc_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    auto ticks = GetTicks();
-    std::string hello = "Hello World! Brought by C++ " + std::to_string(ticks);
+    std::string hello = "Hello World! Brought by C++";
     return env->NewStringUTF(hello.c_str());
 }
